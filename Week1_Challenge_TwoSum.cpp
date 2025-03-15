f75566f5b67ae0eb6f3b8ff3cb0380da8a0d40d8
Yoni M <programmingwithyoyo@gmail.com>
2025-03-15 12:45:49 -0800

    Project added

---
+++ Week1_Challenge_TwoSum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> num_map;  
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
+            }
+
+            num_map[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int Main (int argc, char *argv[]) {
+    Solution sol;  
+
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> result1 = sol.twoSum(nums1, target1);
+    cout << "Output: [" << result1[0] << ", " << result1[1] << "]" << endl;
+
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> result2 = sol.twoSum(nums2, target2);
+    cout << "Output: [" << result2[0] << ", " << result2[1] << "]" << endl;
+
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> result3 = sol.twoSum(nums3, target3);
+    cout << "Output: [" << result3[0] << ", " << result3[1] << "]" << endl;
+
+    return 0;
+}
